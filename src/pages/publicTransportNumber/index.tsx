cdf51f115b63486f0f9070bad8af4a77e331a333
aclaraaraujo <ac.anaaraujo@gmail.com>
2022-08-11 22:57:59 -0300

    forms create

---
+++ src/pages/publicTransportNumber/index.tsx
@@ -0,0 +1,19 @@
+import styles from "../publicTransportNumber/styles.module.scss";
+
+export default function publicTransportNumber() {
+  return (
+    <section>
+      <span className="title">TRANSPORTE URBANO</span>
+      <div className={styles.publicTransportContainer}>
+        <span>Números do transporte coletivo</span>
+        <p>
+          Como concessionária do serviço público de transporte coletivo em
+          Catalão, a Transduarte apresenta mensalmente números relativos às
+          viagens no perímetro urbano:
+        </p>
+      </div>
+
+      <div className={styles.calendarContainer}></div>
+    </section>
+  );
+}
--- src/pages/sapOmbudsman/index.tsx
+++ src/pages/sapOmbudsman/index.tsx
@@ -1,87 +1,255 @@
+import Link from "next/link";
+import { Facebook, Instagram, Linkedin } from "react-feather";
+import styles from "../sapOmbudsman/styles.module.scss";
+
 export default function sapOmbudsman() {
   return (
     <section>
       <span className="title">TRANSPORTE URBANO</span>
 
-      <div className="formContainer">
-        <p>Nos envie uma mensagem</p>
-        <div className="formContent">
-          <div className="formInput">
-            <article>
-              <label htmlFor="">Nome</label>
-              <input type="text" placeholder="Digite seu nome completo" />
-            </article>
+      <div className={styles.sapContent}>
+        <article>
+          <span>SAP/Ouvidoria</span>
+          <p>
+            Dar opção para Serviço de Atendimento ao Passageiro ou Ouvidoria.
+            Dependendo da escolha, aparecem as informações:
+          </p>
+        </article>
 
-            <article>
-              <label htmlFor="">E-mail</label>
-              <input type="text" placeholder="Digite seu e-mail" />
-            </article>
+        <article>
+          <span>SAP</span>
+          <p>
+            Serviço exclusivo para: dúvidas e informações sobre como recarregar
+            cartão, tarifa, horários e linhas, reclamação, sugestão e elogios.
+          </p>
+          <p>Importante: exija o número do protocolo.</p>
+        </article>
 
-            <article>
-              <label htmlFor="">Telefone</label>
-              <input type="text" placeholder="(64) 9XXXX-XXXX" />
-            </article>
+        <div>
+          <span>Horário</span>
 
-            <article>
-              <label htmlFor="">Assunto</label>
-              <input type="text" placeholder="Assunto" />
-            </article>
+          <p>
+            <strong>Funcionamento:</strong> segunda-feira a sexta-feira (exceto
+            feriado)
+          </p>
+          <p>
+            <strong>Horário:</strong> 6h às 18h (exceto feriado)
+          </p>
+        </div>
 
-            <article>
-              <label htmlFor="">Mensagem</label>
-              <input type="text" placeholder="Mensagem" />
-            </article>
+        <div>
+          <span>Contato</span>
 
-            <article>
-              <label htmlFor="">Currículo</label>
-            </article>
+          <p>
+            <strong>Whatsapp:</strong> (64) 98441-2299 (ligação a cobrar: 90 +
+            14 + número)
+          </p>
+
+          <p>
+            <strong>E-mail:</strong> sap@transduartego.com.br
+          </p>
+          <div className={styles.socialMedia}>
+            <p>Redes sociais: </p>
+            <div>
+              <Link href="/">
+                <article>
+                  <Facebook />
+                  <p>Facebook</p>
+                </article>
+              </Link>
+
+              <Link href="/">
+                <article>
+                  <Instagram />
+                  <p>Instagram</p>
+                </article>
+              </Link>
+
+              <Link href="/">
+                <article>
+                  <Linkedin />
+                  <p>Linkedin</p>
+                </article>
+              </Link>
+            </div>
           </div>
+        </div>
 
-          <button>Enviar mensagem</button>
+        <div className="formContainer">
+          <p>Nos envie uma mensagem</p>
+
+          <div className="formContent">
+            <div className="formInput">
+              <article>
+                <label htmlFor="">Nome</label>
+                <input type="text" placeholder="Digite seu nome completo" />
+              </article>
+
+              <article>
+                <label htmlFor="">E-mail</label>
+                <input type="text" placeholder="Digite seu e-mail" />
+              </article>
+
+              <article>
+                <label htmlFor="">Telefone</label>
+                <input type="text" placeholder="(64) 9XXXX-XXXX" />
+              </article>
+
+              <article>
+                <label htmlFor="">Assunto</label>
+                <input type="text" placeholder="Assunto" />
+              </article>
+
+              <article>
+                <label htmlFor="">Mensagem</label>
+                <input type="text" placeholder="Mensagem" />
+              </article>
+
+              <article>
+                <label htmlFor="">Currículo</label>
+              </article>
+            </div>
+
+            <button>Enviar mensagem</button>
+          </div>
         </div>
       </div>
 
-      <div className="formContainer">
-        <p>Nos envie uma mensagem</p>
-        <div className="formContent">
-          <div className="formInput">
-            <article>
-              <label htmlFor="">Nome</label>
-              <input type="text" placeholder="Digite seu nome completo" />
-            </article>
+      <div className={styles.ombudsmanContent}>
+        <article>
+          <span>Ouvidoria</span>
+          <p>
+            Serviço exclusivo para: Resolver solicitações que não foram
+            atendidas pelo SAP (Serviço de Atendimento ao Passageiro).
+          </p>
+          <p>
+            Pedimos 48 horas para apuração do fato, a partir do momento do
+            contato, exceto sábado, domingo e feriado.
+          </p>
+          <p>
+            Para reclamação na Ouvidoria, é preciso ter o número do protocolo do
+            SAP.
+          </p>
+        </article>
 
-            <article>
-              <label htmlFor="">E-mail</label>
-              <input type="text" placeholder="Digite seu e-mail" />
-            </article>
+        <div>
+          <span>Horário</span>
 
-            <article>
-              <label htmlFor="">Telefone</label>
-              <input type="text" placeholder="(64) 9XXXX-XXXX" />
-            </article>
-
-            <article>
-              <label htmlFor="">Número do protoloco SAP</label>
-              <input type="text" placeholder="Protoloco SAP" />
-            </article>
-
-            <article>
-              <label htmlFor="">Assunto</label>
-              <input type="text" placeholder="Assunto" />
-            </article>
-
-            <article>
-              <label htmlFor="">Mensagem</label>
-              <input type="text" placeholder="Mensagem" />
-            </article>
-
-            <article>
-              <label htmlFor="">Currículo</label>
-            </article>
-          </div>
-
-          <button>Enviar mensagem</button>
+          <p>
+            <strong>Funcionamento:</strong> segunda-feira a sexta-feira (exceto
+            feriado)
+          </p>
+          <p>
+            <strong>Horário:</strong> 6h às 18h (exceto feriado)
+          </p>
         </div>
+
+        <div>
+          <span>Contato</span>
+
+          <p>
+            <strong>Whatsapp:</strong> (64) 98441-2299 (ligação a cobrar: 90 +
+            14 + número)
+          </p>
+
+          <p>
+            <strong>E-mail:</strong> sap@transduartego.com.br
+          </p>
+          <p>
+            <strong>Whatsapp:</strong> sap@transduartego.com.br
+          </p>
+        </div>
+
+        <div className="formContainer">
+          <p>Nos envie uma mensagem</p>
+          <div className="formContent">
+            <div className="formInput">
+              <article>
+                <label htmlFor="">Nome</label>
+                <input type="text" placeholder="Digite seu nome completo" />
+              </article>
+
+              <article>
+                <label htmlFor="">E-mail</label>
+                <input type="text" placeholder="Digite seu e-mail" />
+              </article>
+
+              <article>
+                <label htmlFor="">Telefone</label>
+                <input type="text" placeholder="(64) 9XXXX-XXXX" />
+              </article>
+
+              <article>
+                <label htmlFor="">Número do protoloco SAP</label>
+                <input type="text" placeholder="Protoloco SAP" />
+              </article>
+
+              <article>
+                <label htmlFor="">Assunto</label>
+                <input type="text" placeholder="Assunto" />
+              </article>
+
+              <article>
+                <label htmlFor="">Mensagem</label>
+                <input type="text" placeholder="Mensagem" />
+              </article>
+
+              <article>
+                <label htmlFor="">Currículo</label>
+              </article>
+            </div>
+
+            <button>Enviar mensagem</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.usefulInformation}>
+        <span>Informações úteis</span>
+        <article>
+          <span>Achados e Perdidos</span>
+          <p>
+            Mantemos por um período de 30 dias documentos e objetos encontrados
+            dentro dos nossos veículos e/ou no terminal de integração. Após esse
+            período, descartamos o material. Quanto aos bens não reclamados,
+            esses serão doados ou descartados. Exigir do atendente que seja
+            registrado seu pedido de verificação. Na retirada, assinar documento
+            referente aos documentos ou objetos.{" "}
+          </p>
+        </article>
+
+        <article>
+          <span>Acidentes</span>
+          <p>
+            Ao reportar acidentes envolvendo nossos veículos, por favor, enviar
+            e-mail para contato@transduartego.com.br. No Assunto do e-mail,
+            escrever: Acidente! No corpo do e-mail, se possível enviar placa,
+            horário, fotos e local do ocorrido do acidente. Obrigatoriamente,
+            conter no e-mail: nome, placa e modelo do veículo do outro envolvido
+            no acidente. Evite enviar dados desnecessários para nossa apuração
+            do caso.
+          </p>
+        </article>
+
+        <article>
+          <span>Trabalhe conosco</span>
+          <p>
+            Não use esse espaço para enviar currículo ou consultar sobre
+            contratação de nossa empresa. O material será descartado
+            automaticamente. Use a sessão Empresa/Trabalhe Conosco. Os anúncios
+            de recrutamento de pessoal serão publicados em nossas redes sociais:
+            Facebook.com/grupotransduarte e Instragram/grupotransduarte.
+          </p>
+        </article>
+
+        <article>
+          <span>Fornecedores</span>
+          <p>
+            Não use esse espaço para oferecer serviço ou peças à nossa empresa.
+            O material será descartado automaticamente. Use a sessão
+            Empresa/Cadastro de fornecedores.
+          </p>
+        </article>
       </div>
     </section>
   );
