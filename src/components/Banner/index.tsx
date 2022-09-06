68c678da8dcb66d3ae0ed25e805fb98417707626
aclaraaraujo <ac.anaaraujo@gmail.com>
2022-09-06 14:21:25 -0300

    commit

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <article>
-        <div>
+        <div className={styles.headerBanner}>
           <span>Grupo Transduarte</span>
           <p>Fretamento executivo e transporte coletivo.</p>
         </div>
@@ -13,7 +13,7 @@ export default function Banner() {
           Contamos com uma frota de 162 veículos e 183 colaboradores para
           desenvolver constantemente a melhor formar de lhe atender.
         </p>
-        <div className="styles.logotipo">
+        <div className={styles.logotipo}>
           <img src="/banner/gt-1.png" alt="Logotipo do Grupo Transduarte" />
           <img src="/banner/gt-2.png" alt="Logotipo do Grupo Transduarte" />
         </div>
