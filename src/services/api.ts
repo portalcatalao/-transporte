dd0a92eb44733e7a63c13dfd98f7591cf8435948
EzequielPires <ezequiel.pires082000@gmail.com>
2022-11-30 14:52:42 -0300

    Commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { parseCookies } from "nookies";
 export function getApi(ctx?: any) {
     const { 'transduarte.token': token } = parseCookies(ctx);
     const api = axios.create({
-        baseURL: 'https://transduarte.herokuapp.com/'
+        baseURL: 'https://app.transduartego.com.br/'
     })
     if (token) api.defaults.headers['Authorization'] = `Bearer ${token}`;
     return api;
